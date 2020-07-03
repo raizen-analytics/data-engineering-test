367637077290250e5f5d08c21b13d106a19cf437
valves <vitoralvesinfo@gmail.com>
2020-07-02 23:58:53 -0300

    add data transformation

--- solution/transform.py
+++ solution/transform.py
@@ -3,10 +3,20 @@ import pandas, logging
 from pprint import pprint
 
 class Transform(object):
-    def __init__(self, data, keyWord):
+    def __init__(self, data, keyWords):
         self.data = data
-        self.keyWord = keyWord
+        self.keyWords = keyWords
+        self.dfs = []
 
-    def findTablePos(self):
-        pandas.read_excel(self.data, index_col=0)
-        pprint(pandas)
+    def getPivotTables(self):
+        sheet = pandas.read_excel(self.data)
+        for keyWord in self.keyWords:
+            df = sheet.iloc[:,1]
+            indexArray = df[sheet.iloc[:,1] == keyWord].index.values
+            index = indexArray[0].item()
+            df = sheet.iloc[index+9:index+23,1:24]
+            df = df.dropna(axis=1, how='all')
+            header = df.iloc[0]
+            df = df[1:]
+            df.columns = header
+            self.dfs.append(df)
