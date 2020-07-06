3df67c8f7a498949b0aa9f887db24c8cd1264bf2
valves <vitoralvesinfo@gmail.com>
2020-07-06 15:58:43 -0300

    added logging infos

--- solution/database.py
+++ solution/database.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, logging
 
 class Database(object):
     def __init__(self, configs):
@@ -9,10 +9,11 @@ class Database(object):
     def createDB(self):
         dbName = self.cfgDB['name']
         self.conn = sqlite3.connect(dbName + '.db')
+        logging.info('started DB')
         self.createTables()
 
     def createTables(self):
-        schema = self.cfgDB['deafultSchema']
+        schema = self.cfgDB['defaultSchema']
         for key in self.cfgKeyWords:
             createQuery = "CREATE TABLE IF NOT EXISTS {0} ({1})"
             columns = []
@@ -20,6 +21,7 @@ class Database(object):
                 columns.append(column + ' ' + type)
             createQuery = createQuery.format(key,','.join(columns))
             self.conn.execute(createQuery)
+            logging.info('table {0} created'.format(key))
 
     def select(self, table, fields=None):
         selectQuery = 'SELECT * FROM {0}'.format(table)
@@ -41,5 +43,6 @@ class Database(object):
         truncateQuery = 'DELETE FROM {0}'.format(table)
         cursor = self.conn.cursor()
         cursor.execute(truncateQuery)
+        logging.info('data truncated from {0}'.format(table))
         self.conn.commit()
         cursor.close()
