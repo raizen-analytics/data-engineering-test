9ffeeda9c8322e9138adfac1d3f9c1e938aadc5c
valves <vitoralvesinfo@gmail.com>
2020-07-05 23:51:24 -0300

    updated parsing

--- solution/transform.py
+++ solution/transform.py
@@ -1,33 +1,43 @@
-import pandas, logging
+import sys, math, pandas, logging
 
 from pprint import pprint
+from datetime import datetime
 
 class Transform(object):
-    def __init__(self, data, keyWords):
-        self.data = data
+    def __init__(self, db, keyWords):
+        self.db = db
         self.keyWords = keyWords
-        self.startColPos = 1
-        self.endColPos = 24
-        self.startRowPos = 9
-        self.endRowPos = 23
-        self.dfs = []
+        self.months = ['Jan', 'Fev','Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
 
-    def getPivotTables(self):
-        sheet = pandas.read_excel(self.data)
-        for keyWord in self.keyWords:
-            df = sheet.iloc[:,1]
-            try:
-                indexArray = df[sheet.iloc[:,1] == keyWord].index.values
-                index = indexArray[0].item()
-                self.selectPivotRange(sheet, index)
-            except:
-                logging.info('keyword: {0} not found in the database'.format(keyWord))
+    def parseSheet(self, df, sheet):
+        self.dataDict = {}
+        dfsRaw = df.parse(sheet)
+        dfs = dfsRaw.dropna(axis=1, how='all')
+        for index, row in dfs.iterrows():
+            uf = row['ESTADO']
+            if not uf in self.dataDict:
+                self.dataDict[uf] = {}
+            self.aggregateMonth(uf, row)
+        self.loadData()
 
-    def selectPivotRange(self, sheet, index):
-        df = sheet.iloc[index+self.startRowPos:index+self.endRowPos,
-                                    self.startColPos:self.endColPos]
-        df = df.dropna(axis=1, how='all')
-        header = df.iloc[0]
-        df = df[1:]
-        df.columns = header
-        self.dfs.append(df)
+    def aggregateMonth(self, uf, row):
+        rowMonths = [month for month in self.months if row.get(month)]
+        for month in rowMonths:
+            self.dataDict[uf]['unit'] = row['UNIDADE']
+            self.dataDict[uf]['product'] = row['COMBUSTÍVEL']
+            self.dataDict[uf]['year_month'] = str(row['ANO']) + '_' + month
+            self.dataDict[uf]['volume'] = row[month]
+            if math.isnan(row[month]):
+                self.dataDict[uf]['volume'] = 0
+            self.dataDict[uf]['created_at'] = datetime.now()
+
+    def loadData(self):
+        pprint(self.dataDict)
+        del self.dataDict
+
+    def run(self):
+        for key in self.keyWords:
+            df = pandas.ExcelFile('files/{0}Sales.xls'.format(key))
+            sheets = df.sheet_names
+            for sheet in sheets:
+                parsed = self.parseSheet(df, sheet)
