fcc87cb3480f8b9b721700c7a635687b33d35806
valves <vitoralvesinfo@gmail.com>
2020-07-05 22:08:22 -0300

    removed extra file

--- solution/extractor.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-class Extractor(object):
-    def __init__(self, url):
-        self.url = url
-
-    def extract(self):
-        response = requests.get(self.url)
-        return response.content
