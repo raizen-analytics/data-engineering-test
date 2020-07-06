ae74537da2da53b46cd1d42129f0d6c2585f3621
valves <vitoralvesinfo@gmail.com>
2020-07-05 22:09:36 -0300

    renewed main and create raizen object

--- solution/main.py
+++ solution/main.py
@@ -1,31 +1,67 @@
 # -*- coding: utf-8 -*-
 
 ### Import Section
-
 # Python built-in modules
-import json,
+import os, json, sys, sqlite3
 
 # Project modules
-import extractor, transform
+import transform, database
 
 # 3rd part modules
-import logging
+import logging, requests, xlwings
 
 ### Code Section
 logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
 
-def importConfig(self):
-    with open("config.json") as file:
-        return json.load(file)
+class Raizen(object):
+    def __init__(self):
+        self.configs = self.importConfigs()
+        self.url = self.configs['wrapper']['url']
+        self.keyWords = self.configs['wrapper']['keyWords']
 
-def main():
-    configs = importConfig()
-    ext = extractor.Extractor(configs['url'])
-    data = ext.extract()
-    # trs = transform.Transform(ext)
-    # lod = load.Load(trs)
-    # keyWord =
-    pprint(data)
+    def importConfigs(self):
+        with open("config.json", encoding='utf-8') as file:
+            return json.load(file)
+
+    def downloadSheet(self):
+        response = requests.get(self.url)
+        content = response.content
+        for key in self.keyWords:
+            with open('files/{0}Sales.xls'.format(key), 'wb') as file:
+                file.write(content)
+
+    def getPivotSourceData(self):
+        app = xlwings.App(visible=False)
+        app.display_alerts = False
+        vba = xlwings.Book('files/vbas.xlsm')
+        for key in self.keyWords:
+            path = 'files/{0}Sales.xls'.format(key)
+            file = xlwings.Book(path)
+            runVBA = vba.macro('{0}PivotSourceData'.format(key))
+            runVBA()
+            file.save()
+
+        apps = xlwings.apps.active
+        apps.quit()
+
+    def startDB(self):
+        db = database.Database(self.configs)
+        db.createDB()
+        db.insert('oil')
+        db.truncate('oil')
+        db.select('oil')
+
+    def prepareData(self):
+        trs = transform.Transform(data,self.keyWords)
+        trs.getPivotTables()
+
+    def run(self):
+        # self.downloadSheet()
+        # self.getPivotSourceData()
+        # self.startDB()
+        self.prepareData()
+        # trs.show()
 
 # if __file__ == "__main__":
-main()
+r = Raizen()
+r.run()
